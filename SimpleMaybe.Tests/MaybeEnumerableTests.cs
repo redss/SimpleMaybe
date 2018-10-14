c14a00a638d20a99e3d5a1010c41bf4e0c21e53f
redss <ryszard.tarajkowski@gmail.com>
2018-10-14 14:33:19 +0200

    Getting some values from enumeration of Maybe.

---
+++ SimpleMaybe.Tests/MaybeEnumerableTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace SimpleMaybe.Tests
+{
+    public class MaybeEnumerableTests
+    {
+        [Test]
+        public void can_get_all_some_values_from_enumeration_of_maybe()
+        {
+            var maybes = new[]
+            {
+                Maybe.Some(10),
+                Maybe.None<int>(),
+                Maybe.Some(20),
+                Maybe.None<int>(),
+                Maybe.Some(30),
+                Maybe.None<int>()
+            };
+
+            maybes.SomeValues().Should().Equal(10, 20, 30);
+        }
+    }
+}
---
+++ SimpleMaybe/MaybeEnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleMaybe
+{
+    public static class MaybeEnumerableExtensions
+    {
+        public static IEnumerable<TValue> SomeValues<TValue>(this IEnumerable<Maybe<TValue>> maybes)
+        {
+            return maybes.SelectMany(maybe => maybe.ToEnumerable());
+        }
+    }
+}
